fedae75f40cbf644461ac1a8830850cedfbebe95
闫鹏凯 <yanpk@seer-group.com>
2022-11-23 12:56:11 +0800

    Init

---
+++ PKEngine/src/PKEngine.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// For use by PKEngine applications
+#include "PKEngine/Application.h"
+
+// ---Entry Point-------------------
+#include "PKEngine/EntryPoint.h"
+// ---------------------------------
