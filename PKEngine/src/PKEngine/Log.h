a5f1abb04239daeaff557d14213ab27addb555c4
Kaisa970 <magicoftotoro@163.com>
2023-02-20 20:08:24 +0800

    remove glfw

--- PKEngine/src/PKEngine/Log.h
+++ PKEngine/src/PKEngine/Log.h
@@ -13,7 +13,7 @@ namespace PKEngine {
 
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
 		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
-
+		
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
 		static std::shared_ptr<spdlog::logger> s_ClientLogger;
